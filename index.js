107e218780f88875cffc5057131abb3d032d90fb
fbrossar25 <florian.brossard25@gmail.com>
2020-09-11 20:05:58 +0200

    WIP games command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, weeklyChannelId} = require('./config.json');
 const client = new Discord.Client();
+// const weeklyChannel = client.channels.get(weeklyChannelId);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -15,22 +16,28 @@ client.on('message', (msg) => {
 client.login(token);
 
 function parseCommand(msg) {
-    const args = msg.content.substr(1).split(' ');
+    const args = msg.content.slice(prefix.length).trim().split(' ');
+    const name = args.shift();
     return {
         msg: msg,
         channel: msg.channel,
-        name: args[0],
-        args:args.slice(1),
+        name: name,
+        args: args,
     };
 }
 
 function command(cmd) {
     switch (cmd.name) {
     case 'ping': {ping(cmd.channel); break;}
+    case 'games': {games(cmd); break;}
     default: console.log(`Unknown command '${cmd.name}' received with args : ${cmd.args} `);
     }
 }
 
 function ping(channel) {
     channel.send('Pong!');
+}
+
+function games(cmd) {
+    cmd.channel.send('WIP - Will check free games on epic games store');
 }
