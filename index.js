b00cd2e08f9fe85343ae2d0c11522461a0c3ad3b
fbrossar25 <florian.brossard25@gmail.com>
2020-09-11 19:29:49 +0200

    Parse command

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const Discord = require('discord.js');
-const dotenv = require('dotenv');
-dotenv.config();
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.channel.send('Pong!');
-  }
+client.on('message', (msg) => {
+    if (msg.content.startsWith(prefix)) {
+        command(parseCommand(msg));
+    }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(token);
+
+function parseCommand(msg) {
+    const args = msg.content.substr(1).split(' ');
+    return {
+        msg: msg,
+        channel: msg.channel,
+        name: args[0],
+        args:args.slice(1),
+    };
+}
+
+function command(cmd) {
+    switch (cmd.name) {
+    case 'ping': {ping(cmd.channel); break;}
+    default: console.log(`Unknown command '${cmd.name}' received with args : ${cmd.args} `);
+    }
+}
+
+function ping(channel) {
+    channel.send('Pong!');
+}
