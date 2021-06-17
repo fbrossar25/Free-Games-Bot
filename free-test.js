402cc39430bb77095fbb10568216096d35513981
fbrossar25 <florian.brossard25@gmail.com>
2021-06-17 19:38:33 +0200

    New fix for epic based on discount price if any

---
+++ free-test.js
@@ -0,0 +1,41 @@
+const moment = require("moment-timezone");
+const response = require("./free.json");
+const responseElm = response.data.Catalog.searchStore.elements;
+
+const hasActiveFreePromotion = (game) => {
+    const currentDate = moment();
+    if (!game.promotions || !Array.isArray(game.promotions.promotionalOffers)) {
+        return false;
+    }
+    let allPromos = [];
+    for (const promosArray of game.promotions.promotionalOffers) {
+        if (Array.isArray(promosArray.promotionalOffers)) {
+            allPromos = allPromos.concat(promosArray.promotionalOffers);
+        }
+    }
+    let hasActivePromo = false;
+    for (const promo of allPromos) {
+        if (!promo.startDate || !promo.endDate) {
+            continue;
+        }
+        const promoStart = moment(promo.startDate);
+        const promoEnd = moment(promo.endDate);
+        if (currentDate.isAfter(promoStart) && currentDate.isBefore(promoEnd)) {
+            hasActivePromo = true;
+            break;
+        }
+    }
+    if (game.price && game.price.totalPrice) {
+        const originalPrice = game.price.totalPrice.originalPrice | 0;
+        const discountPrice = game.price.totalPrice.discountPrice | 0;
+        const isFree = originalPrice - discountPrice <= 0;
+        return hasActivePromo && isFree;
+    } else {
+        return hasActivePromo;
+    }
+};
+
+for (const game of responseElm) {
+    const isFree = hasActiveFreePromotion(game);
+    console.info(`${game.title} if free ? ${isFree}`);
+}
--- stores.js
+++ stores.js
@@ -1,7 +1,7 @@
-const Utils = require('./utils');
-const axios = require('axios');
-const moment = require('moment');
-const { now } = require('moment');
+const Utils = require("./utils");
+const axios = require("axios");
+const moment = require("moment");
+const { now } = require("moment");
 
 class Store {
     constructor(source, humanSource, url, gamesFromResponseFct, isFreeFct, gameNameFct, gameUrlFct) {
@@ -16,12 +16,13 @@ class Store {
 
     fetch() {
         return new Promise((success) => {
-            axios.get(this.url)
-                .then(response => {
+            axios
+                .get(this.url)
+                .then((response) => {
                     const games = this.gamesFromResponseFct(response);
                     const freeGames = [];
-                    for(const game of games) {
-                        if(this.isFreeFct(game)) {
+                    for (const game of games) {
+                        if (this.isFreeFct(game)) {
                             freeGames.push({
                                 source: this.source,
                                 name: this.gameNameFct(game),
@@ -33,7 +34,7 @@ class Store {
                         games: freeGames,
                     });
                 })
-                .catch(error => {
+                .catch((error) => {
                     Utils.log(`Error ${error.response.status} from ${this.humanSource} : ${JSON.stringify(error)}`);
                     success({
                         source: this.source,
@@ -45,68 +46,82 @@ class Store {
 }
 
 module.exports.epicStore = new Store(
-    'epic',
-    'Epic Games Store',
-    'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=fr&country=FR&allowCountries=FR',
-    response => response.data.data.Catalog.searchStore.elements,
-    game => {
+    "epic",
+    "Epic Games Store",
+    "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=fr&country=FR&allowCountries=FR",
+    (response) => response.data.data.Catalog.searchStore.elements,
+    (game) => {
         const currentDate = moment();
-        if(!(game.promotions) || !Array.isArray(game.promotions.promotionalOffers)) {
+        if (!game.promotions || !Array.isArray(game.promotions.promotionalOffers)) {
             return false;
         }
         let allPromos = [];
-        for(const promosArray of game.promotions.promotionalOffers) {
-            if(Array.isArray(promosArray.promotionalOffers)) {
+        for (const promosArray of game.promotions.promotionalOffers) {
+            if (Array.isArray(promosArray.promotionalOffers)) {
                 allPromos = allPromos.concat(promosArray.promotionalOffers);
             }
         }
         let hasActivePromo = false;
-        for(const promo of allPromos) {
-            if(!(promo.startDate) || !(promo.endDate)) {
+        for (const promo of allPromos) {
+            if (!promo.startDate || !promo.endDate) {
                 continue;
             }
             const promoStart = moment(promo.startDate);
             const promoEnd = moment(promo.endDate);
-            if(currentDate.isAfter(promoStart) && currentDate.isBefore(promoEnd)) {
+            if (currentDate.isAfter(promoStart) && currentDate.isBefore(promoEnd)) {
                 hasActivePromo = true;
                 break;
             }
         }
-        return hasActivePromo && (game.seller) && game.seller.id === 'o-ufmrk5furrrxgsp5tdngefzt5rxdcn';
+        if (game.price && game.price.totalPrice) {
+            const originalPrice = game.price.totalPrice.originalPrice | 0;
+            const discountPrice = game.price.totalPrice.discount | 0;
+            const isFree = originalPrice - discountPrice <= 0;
+            console.log(
+                `${game.title} is priced ${originalPrice} - ${discountPrice} = ${
+                    originalPrice - discountPrice
+                }, hasPromo = ${hasActivePromo}`
+            );
+            return hasActivePromo && isFree;
+        } else {
+            console.log(`${game.title} as no price and hasActivePromo = ${hasActivePromo}`);
+            return hasActivePromo;
+        }
     },
-    game => game.title,
-    game => `https://www.epicgames.com/store/fr/product/${game.productSlug}`);
+    (game) => game.title,
+    (game) => `https://www.epicgames.com/store/fr/product/${game.productSlug}`
+);
 
 module.exports.humbleBundleStore = new Store(
-    'humble',
-    'Humble Bundle',
-    'https://www.humblebundle.com/store/api/search?sort=discount&filter=all&hmb_source=store_navbar&request=1&page=0',
-    response => response.data.results,
-    game => {
+    "humble",
+    "Humble Bundle",
+    "https://www.humblebundle.com/store/api/search?sort=discount&filter=all&hmb_source=store_navbar&request=1&page=0",
+    (response) => response.data.results,
+    (game) => {
         const saleEnd = game.sale_end ? moment(game.sale_end) : null;
-        if(game.current_price.amount === 0 && !game.human_name.endsWith(' Demo')) {
-            if(saleEnd !== null && moment().isAfter(saleEnd)) {
+        if (game.current_price.amount === 0 && !game.human_name.endsWith(" Demo")) {
+            if (saleEnd !== null && moment().isAfter(saleEnd)) {
                 // if sale never end then its a lifetime free game, no point in notifying it
                 return false;
-            }
-            else {
+            } else {
                 // free game
                 return true;
             }
-        }
-        else {
+        } else {
             // not free game
             return false;
         }
     },
-    game => game.human_name,
-    game => `https://www.humblebundle.com/store/${game.human_url}`);
+    (game) => game.human_name,
+    (game) => `https://www.humblebundle.com/store/${game.human_url}`
+);
 
 module.exports.gogStore = new Store(
-    'gog',
-    'GOG',
-    'https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&price=discounted&sort=popularity&hide=dlc',
-    response => response.data.products,
-    game => game.price.isFree && game.price.isDiscounted && !game.title.endsWith(' Demo'),
-    game => game.title,
-    game => `https://www.gog.com${game.url}`);
+    "gog",
+    "GOG",
+    "https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&price=discounted&sort=popularity&hide=dlc",
+    (response) => response.data.products,
+    (game) => game.price.isFree && game.price.isDiscounted && !game.title.endsWith(" Demo"),
+    (game) => game.title,
+    (game) => `https://www.gog.com${game.url}`
+);
